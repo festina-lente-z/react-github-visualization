dd42b64a39c8375a7f204948780f9adabede69ac
BellaZZZZZ <bellazhao_321@qq.com>
2021-07-22 23:14:05 +0800

    饼图

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,7 +3,46 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const {repos} = React.useContext(GithubContext);
+  let languages = repos.reduce((total, item)=>{
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = {label:language,value:1};
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1
+      };
+    }  
+    return total;
+  },{})
+  languages = Object.values(languages).sort((a,b)=>{
+    return b.value - a.value;
+  });
+  console.log(languages);
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13"
+    },
+    {
+      label: "CSS",
+      value: "160"
+    },
+    {
+      label: "JavaScript",
+      value: "80"
+    }
+  ];
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {/*<ExampleChart data={chartData} />*/}
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
@@ -18,7 +57,7 @@ const Wrapper = styled.div`
     grid-template-columns: 2fr 3fr;
   }
 
-  div {
+  /* div {
     width: 100% !important;
   }
   .fusioncharts-container {
@@ -27,7 +66,7 @@ const Wrapper = styled.div`
   svg {
     width: 100% !important;
     border-radius: var(--radius) !important;
-  }
+  } */
 `;
 
 export default Repos;
